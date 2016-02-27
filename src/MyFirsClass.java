f364c8f620129ac600c56385a1d6af2076053958
VasylchenkoS <Vasylchenko.S@gmail.com>
2016-02-27 16:12:11 +0200

    MainFiles
    first commit for thith project

---
+++ src/MyFirsClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by v.vasilchenko on 27.02.2016.
+ */
+public class MyFirsClass {
+    public static void main(String[] args){
+        System.out.println("Hello World :)");
+    }
+}
