dcacaf36adfbf9765f925a9a1353102ca4e258f1
VasylchenkoS <Vasylchenko.S@gmail.com>
2016-02-29 22:55:26 +0200

    0

--- src/vasylchenko/Module1/Module1.java
+++ src/vasylchenko/Module1/Module1.java
@@ -4,6 +4,6 @@
 package vasylchenko.Module1;
 public class Module1 {
     public static void main(String[] args){
-        System.out.println("Hello World :)");
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
     }
 }
