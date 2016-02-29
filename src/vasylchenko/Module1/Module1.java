30aaf687c53d8ddfc8ad89e28dbac80f4c00dcb7
VasylchenkoS <Vasylchenko.S@gmail.com>
2016-02-29 09:44:38 +0200

    Change1

--- src/vasylchenko/Module1/Module1.java
+++ src/vasylchenko/Module1/Module1.java
@@ -1,7 +1,8 @@
 /**
  * Created by v.vasilchenko on 27.02.2016.
  */
-public class MyFirsClass {
+package vasylchenko.Module1;
+public class Module1 {
     public static void main(String[] args){
         System.out.println("Hello World :)");
     }
