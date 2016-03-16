a5781b2726611901e8449bb95f4aca1a2d0214f8
VasylchenkoS <Vasylchenko.S@gmail.com>
2016-03-16 19:51:05 +0200

    Module5 V1.0

--- src/vasylchenko/module4/mod4_1/FigureArea.java
+++ src/vasylchenko/module4/mod4_1/FigureArea.java
@@ -1,54 +1,36 @@
 package vasylchenko.module4.mod4_1;
 
-import java.io.IOException;
-import java.util.Scanner;
-
 /**
  * Created by v.vasilchenko on 09.03.2016.
  */
 public class FigureArea {
-
-    public static void main(String[] args) throws IOException {
-        FigureArea figureArea = new FigureArea();
-        figureArea.getData();
-    }
-
-    public void getData() {
-        double a, b, c;
-        System.out.print("Введите данные: ");
-        Scanner sc1 = new Scanner(System.in);
-        a = sc1.nextDouble();
-        Scanner sc2 = new Scanner(System.in);
-        b = sc2.nextDouble();
-        Scanner sc3 = new Scanner(System.in);
-        c = sc3.nextDouble();
-        FigureArea fig = new FigureArea();
-        if ((a != 0) && (b == 0) && (c == 0))
-            fig.figureArea(a);
-        else if ((a != 0) && (b != 0) && (c == 0))
-            fig.figureArea(a, b);
-        else if ((a != 0) && (b != 0) && (c != 0))
-            fig.figureArea(a, b, c);
-    }
 
     private void figureArea()
     {
         System.out.println("Введено мало аргументов");
     }
 
-    private void figureArea(double radius) {
-        double area = Math.PI*radius*radius;
-        //Площадь круга вычисляется по формуле: S = Pi*Radius^2
-        System.out.println("Площадь круга равна " + area);
+    private void circleArea(double radius) {
+        if (radius <= 0) {
+            System.out.println("Круга с радиусом " + radius + " не существует");
+        } else {
+            double area = Math.PI * radius * radius;
+            //Площадь круга вычисляется по формуле: S = Pi*Radius^2
+            System.out.println("Площадь круга равна " + area);
+        }
     }
 
-    private void figureArea(double sideA, double sideB) {
-        double area = sideA*sideB;
-        //Площадь прямоугольника вычисляется по формуле: S = sideA * sideB
-        System.out.println("Площадь прямоугольника равна " + area);
+    private void rectangleArea(double sideA, double sideB) {
+        if (sideA <= 0 || sideB <= 0) {
+            System.out.println("Прямоугольника со сторонами " + sideA + " " + sideB + " не существует");
+        } else {
+            double area = sideA * sideB;
+            //Площадь прямоугольника вычисляется по формуле: S = sideA * sideB
+            System.out.println("Площадь прямоугольника равна " + area);
+        }
     }
 
-    private void figureArea(double sideA, double sideB, double sideC) {
+    private void triangleArea(double sideA, double sideB, double sideC) {
         //проверим существование треугольника:
         if ((sideA<sideB+sideC)&&(sideB<sideA+sideC)&&(sideC<sideA+sideB)){
             double p = (sideA+sideB+sideC)/2;
