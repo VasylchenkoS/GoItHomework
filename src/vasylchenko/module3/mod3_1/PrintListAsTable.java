774b77ff4507488a056e509a101ebbb11a8a6b97
VasylchenkoS <Vasylchenko.S@gmail.com>
2016-03-26 18:48:19 +0200

    Module8 V1.0

--- src/vasylchenko/module3/mod3_1/PrintListAsTable.java
+++ src/vasylchenko/module3/mod3_1/PrintListAsTable.java
@@ -1,24 +1,15 @@
 package vasylchenko.module3.mod3_1;
 
-import java.util.List;
-import java.util.Set;
+import java.util.Collection;
 
 /**
  * Created by v.vasilchenko on 26.03.2016.
  */
 public class PrintListAsTable {
-    PrintListAsTable(List<File> printList) {
-        System.out.printf("|%-30s|%-10s|%-10s%n", "Name", "Type", "Size");
+    PrintListAsTable(Collection<File> printList) {
+        System.out.printf("||%-30s||%-10s||%-10s%n", "Name", "Type", "Size");
         for (File file : printList) {
-            System.out.printf("|%-30s|%-10s|%-10d%n", file.getName(), file.getType(), file.getSize());
-        }
-        System.out.println("\n");
-    }
-
-    PrintListAsTable(Set<File> printList) {
-        System.out.printf("|%-30s|%-10s|%-10s%n", "Name", "Type", "Size");
-        for (File file : printList) {
-            System.out.printf("|%-30s|%-10s|%-10d%n", file.getName(), file.getType(), file.getSize());
+            System.out.printf("||%-30s||%-10s||%-10d%n", file.getName(), file.getType(), file.getSize());
         }
         System.out.println("\n");
     }
