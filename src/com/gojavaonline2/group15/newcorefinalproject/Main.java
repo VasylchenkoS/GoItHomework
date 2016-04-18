328ab1b8597430a020d09079da0d72107331af63
VasylchenkoS <Vasylchenko.S@gmail.com>
2016-04-18 21:36:35 +0300

    Final Project With Comments

--- src/com/gojavaonline2/group15/newcorefinalproject/Main.java
+++ src/com/gojavaonline2/group15/newcorefinalproject/Main.java
@@ -4,8 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         Tree<Integer> tree = new Tree<Integer>().add(7).add(8)
-                .add(10).add(5).add(6).add(4)
+                .add(9).add(5).add(6).add(4)
                 .add(2).add(3).add(1);
+//      В первую очередь добавляется 7. Это значение становится корне дерева.
+//      Затем мы добавляем 8. Поскольку 7 меньше 8, мы кладем ее в правое поддерево.
+//      таким-же образом 9 становится правым поддеревом корня 8.
+//      5 становится левым ребенком корня 7. 6 становится правым ребенком 5
+//      Последовательное сравнение вставляемого значения с потенциальным родителем продолжается до тех пор,
+//      пока не будет найдено место для вставки, и повторяется для каждого вставляемого значения до тех пор,
+//      пока не будет построено все дерево целиком.
+
+//      далее проверяем обход дерева тремя методами с выводом результата на экран
         Traverse.preorderTraverse(tree.getNode());
         System.out.println("\nInorder Traverse Procedure Completed");
         Traverse.inorderTraverse(tree.getNode());
@@ -13,8 +22,11 @@ public class Main {
         Traverse.postorderTraverse(tree.getNode());
         System.out.println("\nPostorder Traverse Procedure Completed");
 
-        Tree<String> tree1 = new Tree<String>().add("node7").add("node8").add("node10").add("node5").add("node6")
-        .add("node4").add("node2").add("node3").add("node1");
+//      изменим тип данных, но сохраним структуру дерева, для упрощения сравнения результатов
+//      и повторим те-же процедуры, что и в предыдущем случае
+        Tree<String> tree1 = new Tree<String>().add("node7").add("node8")
+                .add("node9").add("node5").add("node6").add("node4")
+                .add("node2").add("node3").add("node1");
         Traverse.preorderTraverse(tree1.getNode());
         System.out.println("\nInorder Traverse Procedure Completed");
         Traverse.inorderTraverse(tree1.getNode());
--- src/com/gojavaonline2/group15/newcorefinalproject/Node.java
+++ src/com/gojavaonline2/group15/newcorefinalproject/Node.java
@@ -1,16 +1,22 @@
 package com.gojavaonline2.group15.newcorefinalproject;
 
+import java.text.Collator;
+import java.util.Locale;
+
+//  В данном классе определена структура дерева
+
 public class Node<T extends Comparable<T>> implements Comparable<T> {
-    private Node<T> left;
-    private Node<T> right;
-    private T value;
+    private Node<T> left;   // указатель на левого потомка
+    private Node<T> right;  // указатель на правого потомка
+    private T value;        // ключ узла
+
+//    создаем констуктор, а так же getter и setter для єлементов класса
 
     public Node(Node<T> left, Node<T> right, T value) {
         this.left = left;
         this.right = right;
         this.value = value;
     }
-
 
     public Node<T> getLeft() {
         return left;
@@ -32,9 +38,27 @@ public class Node<T extends Comparable<T>> implements Comparable<T> {
         return value;
     }
 
+//  создаем метод для сравнения переменных внутри дерева
 
-    public int compareTo(T o) {
-        return o.compareTo(value);
+    public int compareTo(T object) {
+//  т.к. метод compareTo() не совсем подъходит для сравнения строк, воспользуемся библлиотечным классом Collator.
+//  Метод compare() выполняет сравнение имен двух файлов методом collator.compare(о1,о2)
+//  Возвращает:
+//  1 если первый параметр (о1) больше второго (о2),
+//  -1 если первый параметр (о1) меньше второго (о2),
+//  0 если они равны.
+//  Имя первого файла считается больше второго имени, если
+//  первый файл находится ближе к корню дерева папок.
+        if (value.getClass() == String.class) {
+            Collator collator = Collator.getInstance(new Locale("en", "US"));
+            return collator.compare(object, value);
+        }
+//  Для типа Integer воспользуемся методом compareTo(о1,о2)
+//  Возвращает:
+//  1 если первый параметр (о1) больше второго (о2),
+//  -1 если первый параметр (о1) меньше второго (о2),
+//  0 если они равны.
+        else return object.compareTo(value);
     }
 }
 
